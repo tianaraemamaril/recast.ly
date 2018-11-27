a5aa04c1da7b5e675db405f232e14d2f6c16964f
tianaraemamaril <tianaraetran@gmail.com>
2018-11-26 20:33:43 -0800

    Renders App. Working on Video List.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,12 @@
-var VideoList = () => (
+//import exampleVideoData from '../data/exampleVideoData.js';
+//import VideoListEntry from '/VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {exampleVideoData.map(video =>
+      <VideoListEntry entry={video}/>
+    )
+    }
   </div>
 );
 
