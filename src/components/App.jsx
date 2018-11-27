8e9b5820e409bdb1c4a5f3a990ec1de4230d76c7
tianaraemamaril <tianaraetran@gmail.com>
2018-11-27 12:11:10 -0800

    Clicking video title plays clicked video in Video Player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,42 @@ import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from '../components/VideoList.js';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={exampleVideoData[0]}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      videos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+  
+  
+  playVideo(video) {
+    this.setState({ currentVideo: video });
+  }
 
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVideo}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} onClickPlay={this.playVideo.bind(this)}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,7 +3,7 @@ import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map(videoEntry =>
-      <VideoListEntry video={videoEntry}/>
+      <VideoListEntry video={videoEntry} onClickPlay={props.onClickPlay}/>
     )
     }
   </div>
