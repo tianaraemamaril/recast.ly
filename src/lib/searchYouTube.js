69db999471ac216d27bd54e663ce09308b458d07
tianaraemamaril <tianaraetran@gmail.com>
2018-11-27 19:45:06 -0800

    Revert "Revert "temp commit with working get""
    This reverts commit c65e5af2f56bc61e7aeeaca9866494dff0907fe6.

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,12 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  console.log(options);
+  
+  $.get({
+    url:'https://www.googleapis.com/youtube/v3/search',
+    data: _.extend(options, { part: 'snippet'}),
+    success: callback    
+  });
 };
 
 export default searchYouTube;
